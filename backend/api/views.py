aea3ce4806125b69211f9fcba548b62856899192
Simeon-Vutov <simeon32.dev@gmail.com>
2023-07-13 14:45:26 +0300

    Modified the api view so now it returns some information for the request that was made

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,18 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({'data': 'Hello World!'})
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+
+    return JsonResponse(data)
