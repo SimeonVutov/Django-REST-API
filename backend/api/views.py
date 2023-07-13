c510f1afe92a098b7f36ef3f27729aa28d91cbe2
Simeon-Vutov <simeon32.dev@gmail.com>
2023-07-13 15:54:22 +0300

    Added decorator to specify which http methods can be used with the api_home view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,9 @@
 from django.http import JsonResponse
 from products.models import Product
+from rest_framework.decorators import api_view
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     random_product = Product.objects.all().order_by("?").first()
     data = {}
