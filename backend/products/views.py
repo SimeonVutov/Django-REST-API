75a689bbc3341dcb0cdf08dde594cc87d0b1bc9c
Simeon-Vutov <simeon32.dev@gmail.com>
2023-07-14 13:20:59 +0300

    Added a function based view for managing products. Added not_found.py to the client system with which I can test the situation where the client requests a product that does not exist.

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,7 @@
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework import generics
 
 from .models import Product
@@ -21,3 +25,34 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == 'GET':
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+
+        return Response(data)
+
+    if method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content')
+
+            if content is None:
+                content = title
+
+            serializer.save(content=content)
+
+            return Response(serializer.data)
+        return Response({'error': 'Invalid data'}, status=400)
