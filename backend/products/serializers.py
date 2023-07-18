123f643f1c992fc9d42274c4c8bc2aec97607dfe
Simeon-Vutov <simeon32.dev@gmail.com>
2023-07-18 10:44:49 +0300

    Added owner and related_products to the ProductSerializer. Used new inline serializer which gets all products owned by the user and and serialize them. Added new serializers.py file to the api app.

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,12 +1,29 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
+from api.serializers import UserPublicSerializer
+
 from . import validators
 
 from .models import Product
 
 
+class ProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk',
+        read_only=True
+    )
+    title = serializers.CharField(read_only=True)
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    owner = UserPublicSerializer(source='user', read_only=True)
+    related_products = ProductInlineSerializer(
+        source='user.product_set.all',
+        read_only=True,
+        many=True
+    )
     discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(
@@ -23,6 +40,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
+            'owner',
             'url',
             'edit_url',
             'pk',
@@ -32,6 +50,7 @@ class ProductSerializer(serializers.ModelSerializer):
             'price',
             'sale_price',
             'discount',
+            'related_products'
         ]
 
     # def validate_title(self, value):
